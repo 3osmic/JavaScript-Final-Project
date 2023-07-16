b187147a46acd07f2c5cd926e2d5a7074ed24f01
Aliyah <aliyah.y.lewis@gmail.com>
2023-07-16 14:25:13 -0400

    More Styling

---
+++ projectJS.js
@@ -0,0 +1,9 @@
+// The meal plan will display the users input for 5 meals (Breakfast, snack, lunch, snack & dinner) for the week (Monday – Sunday)
+
+// The user will have the ability to clear the weekly planner
+
+// User should also have the ability to print or download planner
+
+// The application must collect the user's personal information name, email & goal for the week
+
+// 
